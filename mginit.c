d1edbab5dd1360ac91da500982e180c72c164361
Vincent Wei <vincent@minigui.org>
2020-02-13 11:41:34 +0800

    add mginit for procs runmode

---
+++ mginit.c
@@ -0,0 +1,138 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+//                          IMPORTANT NOTICE
+//
+// The following open source license statement does not apply to any
+// entity in the Exception List published by FMSoft.
+//
+// For more information, please visit:
+//
+// https://www.fmsoft.cn/exception-list
+//
+//////////////////////////////////////////////////////////////////////////////
+/* 
+** $Id: mginit.c 322 2007-08-30 01:20:10Z xwyan $
+**
+** Listing 31.1
+**
+** mginit.c: Sample program for MiniGUI Programming Guide
+**      A simple mginit program.
+**
+** Copyright (C) 2003 ~ 2017 FMSoft (http://www.fmsoft.cn).
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
+**
+**     http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include <minigui/common.h>
+#include <minigui/minigui.h>
+#include <minigui/gdi.h>
+#include <minigui/window.h>
+
+static BOOL quit = FALSE;
+
+static void on_new_del_client (int op, int cli)
+{       
+    static int nr_clients = 0;
+
+    if (op == LCO_NEW_CLIENT) {
+        nr_clients ++;
+    }   
+    else if (op == LCO_DEL_CLIENT) {
+        nr_clients --;
+        if (nr_clients == 0) {
+            printf ("There is no any client, I will quit.\n");
+            quit = TRUE;
+        }               
+        else if (nr_clients < 0) {
+            printf ("Serious error: nr_clients less than zero.\n");
+        }
+    }
+    else
+        printf ("Serious error: incorrect operations.\n");
+}
+
+static pid_t exec_app (const char* file_name, const char* app_name)
+{
+    pid_t pid = 0;
+
+    if ((pid = vfork ()) > 0) {
+        fprintf (stderr, "new child, pid: %d.\n", pid);
+    }
+    else if (pid == 0) {
+        execl (file_name, app_name, NULL);
+        perror ("execl");
+        _exit (1);
+    }
+    else {
+        perror ("vfork");
+    }
+
+    return pid;
+}
+
+static void child_wait (int sig)
+{
+    int pid;
+    int status;
+
+    while ((pid = waitpid (-1, &status, WNOHANG)) > 0) {
+        if (WIFEXITED (status))
+            printf ("--pid=%d--status=%x--rc=%d---\n", pid, status, WEXITSTATUS(status));
+        else if (WIFSIGNALED(status))
+            printf ("--pid=%d--signal=%d--\n", pid, WTERMSIG (status));
+    }
+}
+
+int MiniGUIMain (int args, const char* arg[])
+{
+    MSG msg;
+    struct sigaction siga;
+
+    siga.sa_handler = child_wait;
+    siga.sa_flags  = 0;
+    memset (&siga.sa_mask, 0, sizeof(sigset_t));
+    sigaction (SIGCHLD, &siga, NULL);
+
+    OnNewDelClient = on_new_del_client;
+
+    if (!ServerStartup (0 , 0 , 0)) {
+        fprintf (stderr, 
+                 "Can not start the server of MiniGUI-Processes: mginit.\n");
+        return 1;
+    }
+
+    if (args > 1) {
+        if (exec_app (arg[1], arg[1]) == 0)
+            return 3;
+    }
+    else {
+        if (exec_app ("./mguxdemo", "mguxdemo") == 0)
+            return 3;
+    }
+
+    while (!quit && GetMessage (&msg, HWND_DESKTOP)) {
+        DispatchMessage (&msg);
+    }
+
+    return 0;
+}
+
